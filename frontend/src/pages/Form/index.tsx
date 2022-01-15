e0968dff71e007ddef2f0ad26867c9ca13a0e74c
Kaue Costa <kauecmoraes@hotmail.com>
2022-01-15 15:59:17 -0300

    Navegation button

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 function Form() {
     const movie = {
@@ -27,11 +28,16 @@ function Form() {
                             <option>5</option>
                         </select>
                     </div>
-                    <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
-                    </div>
+                    <Link to="/">
+                        <div className="dsmovie-form-btn-container">
+                            <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                        </div>
+                    </Link>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
 
